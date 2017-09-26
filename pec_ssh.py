5c50c63672f4bf6322ad82e7e47989a471724481
gggggaoyu <xiangqukk@163.com>
2017-09-26 10:32:00 +0800

    修改登录函数

--- pec_ssh.py
+++ pec_ssh.py
@@ -3,7 +3,7 @@
 
 import pexpect
 import getpass,os
-import logging
+import logging,re
 from pexpect import spawn as sp
 
 def ssh_command(command):
@@ -16,36 +16,43 @@ def ssh_command(command):
        connect to a new host and ssh asks you if you want to accept the public key
        fingerprint and continue connecting.
        """
-    ssh_newkey = 'Are you sure you want to continue connecting'
     # 为 ssh 命令生成一个 spawn 类的子程序对象.
-    child = sp('ssh -l %s %s %s' % (account_name,manage_ip,command), maxread=2000)
-    # child.send('\n')
-    i = child.expect([pexpect.TIMEOUT, ssh_newkey, 'password: '])
+    child = sp('ssh -l %s %s' % (account_name, manage_ip), maxread=2000)
+    ssh_newkey = 'Are you sure you want to continue connecting'
+    Password = 'password: '
+    loginprompt = '.*?[$#>]'
+    i = child.expect([pexpect.TIMEOUT, ssh_newkey, Password,loginprompt])
+
     # 如果登录超时，打印出错信息，并退出.
     if i == 0:  # Timeout
-        print 'ERROR!'
+        print 'TIMEOUT!'
         print 'SSH could not login. Here is what SSH said:'
         print child.before, child.after
         return None
+
     # 如果 ssh 没有 public key，接受它.
     if i == 1:  # SSH does not have the public key. Just accept it.
         child.sendline('yes')
-        child.expect('password: ')
-        i = child.expect([pexpect.TIMEOUT, 'password: '])
-        if i == 0:  # Timeout
-            print 'ERROR!'
+        p = child.expect([pexpect.TIMEOUT, 'password: '])
+        if p == 0:  # Timeout
+            print 'TIMEOUT!'
             return None
-        print 'SSH could not login. Here is what SSH said:'
-        print child.before, child.after
-        if i == 1:
+            print 'SSH could not login. Here is what SSH said:'
+            print child.before, child.after
+        if p == 1:
             child.sendline('JCFWnantian2014')
+
     # 输入密码.
     if i == 2:
         child.sendline('JCFWnantian2014')
-    return child
+
+    else:
+        return child
 
 if __name__ == '__main__':
     command_a = 'show version'
     command_b = 'show ip interface brief'
     child = ssh_command(command_b)
     print child.before
+    print 80*"{}"
+    print child.after
